72a6b10bf305cdbf45d195c0f8d8ab44433ddfda
IvanBYA <ivan_yakab@hotmail.com>
2022-11-27 23:08:20 -0600

    Autocompletado de código Cypress creado

--- cypress/e2e/testingAutomation.cy.js
+++ cypress/e2e/testingAutomation.cy.js
@@ -1,17 +1,16 @@
 describe('Plan de Pruebas Tienda en Linea - (Automation Exercise)', () => {
   // TEST CASE 1
-  it('Registrar Usuario', () => {
+  it('Registro de usuario y Cierre de Sesión', () => {
 
     const userValue = {
       nombre: "Juan Antonio prueba",
       correo: "prueba@estoesunaprueba.prueba"
     }
-
     cy.visit('/');
     cy.get('.shop-menu > .nav > :nth-child(4) > a').
       click();
     cy.url().
-      should('include','/login');
+      should('include', '/login');
     cy.get('.signup-form > h2').
       contains('New User Signup');
     cy.get('[type="text"]').
@@ -23,20 +22,20 @@ describe('Plan de Pruebas Tienda en Linea - (Automation Exercise)', () => {
     cy.get(':nth-child(1) > b').
       // should('have.text','Enter Account Information');
       // should('include.text','Enter Account Information');
-    contains('Enter Account Information');
+      contains('Enter Account Information');
     cy.get('#id_gender1').
       check();
     cy.get('#password').
       type('contraseñaprueba');
     cy.get('#days').
       select('1').
-      should('have.value','1');
+      should('have.value', '1');
     cy.get('#months').
       select('June').
-      should('have.value','6');
+      should('have.value', '6');
     cy.get('#years').
       select('1985').
-      should('have.value','1985');
+      should('have.value', '1985');
     cy.get('#newsletter').
       check();
     cy.get('#optin').
@@ -53,7 +52,7 @@ describe('Plan de Pruebas Tienda en Linea - (Automation Exercise)', () => {
       type('Dirección 2');
     cy.get('#country').
       select('Canada').
-      should('have.value','Canada');
+      should('have.value', 'Canada');
     cy.get('#state').
       type('prueba');
     cy.get('#city').
@@ -65,16 +64,16 @@ describe('Plan de Pruebas Tienda en Linea - (Automation Exercise)', () => {
     cy.get('.login-form > form > .btn').
       click();
     cy.get('b').
-      should('have.text','Account Created!');
+      should('have.text', 'Account Created!');
     cy.get('.pull-right > .btn').
       click();
     cy.get(':nth-child(10) > a').
-      should('include.text','Logged in as ' + String(userValue.nombre));
+      should('include.text', 'Logged in as ' + String(userValue.nombre));
     cy.get('.shop-menu > .nav > :nth-child(5) > a').
       click();
     cy.get('b').
-      should('have.text','Account Deleted!');
+      should('have.text', 'Account Deleted!');
     cy.get('.pull-right > .btn').
       click();
-    })
+  })
 })
